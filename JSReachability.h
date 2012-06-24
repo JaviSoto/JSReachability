e7c20edb993a1a63614ab0a31f12314a8bc9506f
Javier Soto <javier.api@gmail.com>
2012-06-23 23:14:50 -0700

    First commit

---
+++ JSReachability.h
@@ -0,0 +1,51 @@
+/* 
+ Copyright 2012 Javier Soto (ios@javisoto.es)
+ 
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+ 
+ http://www.apache.org/licenses/LICENSE-2.0
+ 
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License. 
+ */
+
+extern NSString * const kJSReachabilityHostDidBecomeReachableNotification;
+extern NSString * const kJSReachabilityHostDidBecomeUnReachableNotification;
+extern NSString * const kJSReachabilityNotificationHostUserInfoKey;
+
+@protocol JSReachabilityDelegate;
+
+@interface JSReachability : NSObject
+
+@property (nonatomic, assign, readonly) BOOL connectionAvailable;
+@property (nonatomic, assign) id<JSReachabilityDelegate> delegate;
+
+/**
+ * @param host the host to monitor whether it's reachable or not
+ * @param delegate (optional)
+ */
++ (JSReachability *)reachabilityWithHost:(NSString *)host
+                                       delegate:(id<JSReachabilityDelegate>)delegate;
+
+/**
+ * @discussion Starts monitoring the provided host for reachability and calls the delegate with updates
+ */
+- (void)start;
+
+/**
+ * @discussion Stops monitoring the provided host for reachability and stops calling the delegate with updates
+ */
+- (void)stop;
+
+@end
+
+@protocol JSReachabilityDelegate <NSObject>
+
+- (void)reachabilityService:(JSReachability *)reachability host:(NSString *)host didBecomeReachable:(BOOL)reachable;
+
+@end
